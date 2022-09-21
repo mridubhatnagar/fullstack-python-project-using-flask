ecccb6346d640149cf2d72a768468bd057927615
mridubhatnagar <mridubhatnagar4@gmail.com>
2022-09-21 12:35:46 +0530

    flask backend for the application

---
+++ app.py
@@ -0,0 +1,36 @@
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def student_information():
+    studinfo = {
+            "U101113FCS103": {
+                "student_name": "Harry Potter",
+                "age": 25,
+                "gender": "Male",
+                "house": "Griffendor",
+            },
+            "U101113FCS104": {
+                "student_name": "Hermione Granger",
+                "age": 27,
+                "gender": "Female",
+                "house": "Griffendor"
+            },
+            "U101113FCS105": {
+                "student_name": "Ron Weasley",
+                "age": 26,
+                "gender": "Male",
+                "house": "Griffendor"
+            },
+            "U101113FCS105": {
+                "student_name": "Albus Dumbeldore",
+                "age": 35,
+                "gender": "Male",
+                "house": "Griffendor"
+
+            }
+    }
+    return jsonify(studinfo)
