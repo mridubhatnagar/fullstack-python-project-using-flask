d52a156eada76d6453fc1cfccaa1047bcc6ee4f8
mridubhatnagar <mridubhatnagar4@gmail.com>
2022-09-21 16:44:02 +0530

    added onclick method to button and modified method in app.py

--- app/app.py
+++ app/app.py
@@ -1,36 +1,45 @@
 import json
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def student_information():
-    studinfo = {
-            "U101113FCS103": {
-                "student_name": "Harry Potter",
-                "age": 25,
-                "gender": "Male",
-                "house": "Griffendor",
-            },
-            "U101113FCS104": {
-                "student_name": "Hermione Granger",
-                "age": 27,
-                "gender": "Female",
-                "house": "Griffendor"
-            },
-            "U101113FCS105": {
-                "student_name": "Ron Weasley",
-                "age": 26,
-                "gender": "Male",
-                "house": "Griffendor"
-            },
-            "U101113FCS105": {
-                "student_name": "Albus Dumbeldore",
-                "age": 35,
-                "gender": "Male",
-                "house": "Griffendor"
+    if request.method == "POST":
+        rollno = request.form.get("rollno")
+        studinfo = {
+                "U101113FCS103": {
+                    "student_name": "Harry Potter",
+                    "age": 25,
+                    "gender": "Male",
+                    "house": "Griffendor",
+                },
+                "U101113FCS104": {
+                    "student_name": "Hermione Granger",
+                    "age": 27,
+                    "gender": "Female",
+                    "house": "Griffendor"
+                },
+                "U101113FCS105": {
+                    "student_name": "Ron Weasley",
+                    "age": 26,
+                    "gender": "Male",
+                    "house": "Griffendor"
+                },
+                "U101113FCS105": {
+                    "student_name": "Albus Dumbeldore",
+                    "age": 35,
+                    "gender": "Male",
+                    "house": "Griffendor"
 
-            }
-    }
-    return render_template("student_information.html", data=jsonify(studinfo))
+                }
+        }
+
+        if rollno in studinfo:
+            data = jsonify(studinfo[rollno])
+        else:
+            data = ""
+        return render_template("student_information.html", data=data)
+    elif request.method == "GET":
+        return render_template("student_information.html")
